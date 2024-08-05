ebd23afdbf0f79eacde4b7f2ca659958f60f018c
Colddave <152074258+Colddave@users.noreply.github.com>
2024-08-05 16:18:25 +0300

    Add files via upload

---
+++ 03-ystyle/script.js
@@ -0,0 +1,49 @@
+// Боковое меню
+
+function openSidemenu() {
+    document.getElementById('overlay-sidemenu').style.width = "100%";
+}
+
+function closeSidemenu() {
+    document.getElementById('overlay-sidemenu').style.width = "0";
+}
+
+// Корзина
+
+function openSidebarCart() {
+    document.getElementById('overlay-cart').style.width = "100%";
+}
+
+function closeSidebarCart() {
+    document.getElementById('overlay-cart').style.width = "0";
+}
+
+// Избранное
+
+function openSidebarFavorite() {
+    document.getElementById('overlay-favorite').style.width = "100%";
+}
+
+function closeSidebarFavorite() {
+    document.getElementById('overlay-favorite').style.width = "0";
+}
+
+// Аккаунт
+
+function openSidebarAccount() {
+    document.getElementById('overlay-account').style.width = "100%";
+}
+
+function closeSidebarAccount() {
+    document.getElementById('overlay-account').style.width = "0";
+}
+
+// Поиск
+
+function openSidebarSearch() {
+    document.getElementById('overlay-search').style.width = "100%";
+}
+
+function closeSidebarSearch() {
+    document.getElementById('overlay-search').style.width = "0";
+}
